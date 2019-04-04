566a4ca5b14b543e2e408d989370dde6f8dadf6e
David Liu <david-lky@hotmail.com>
2019-04-04 02:40:30 -0400

    working server

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json');
+const DataJson = require('./data.json');
 const middlewares = jsonServer.defaults({
   static: 'public/',
 });
 
 const port = process.env.PORT || 3000;
-
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
+
+server.get('/get_data', (req, res) => {
+  if (req.body.type) {
+    res.jsonp(DataJson[req.body.type.substr(3).toLowerCase()]);
+  } else {
+    req.jsonp(DataJson);
+  }
+});
 server.use(router);
 server.listen(port, () => {
   console.log('JSON Server is running');
